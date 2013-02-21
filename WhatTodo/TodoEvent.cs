44c105455e9760da159e1a40ada3eb8005409678
Lauri Lavanti <lauri.lavanti@aalto.fi>
2013-02-21 20:00:32 +0200

    Basis for TodoEvent and TodoList
    Mainly for enumeration in TodoEvents

---
+++ WhatTodo/TodoEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WhatTodo
+{
+    public enum Priority 
+    {
+        Now,
+        DL,
+        Def,
+        Cal
+    }
+    class TodoEvent
+    {
+        public string Name { get; set; }
+        
+    }
+}
