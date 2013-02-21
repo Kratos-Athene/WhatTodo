ae2baf6b9a999e865bc625f74643f2f8bea289be
Lauri Lavanti <lauri.lavanti@aalto.fi>
2013-02-21 23:39:52 +0200

    Began working on TimeList class

---
+++ WhatTodo/TimeList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WhatTodo {
+	class TimeList {
+		public List<Time> Times { get; set; }
+
+		public TimeList(List<Event> ListOfEvents) {
+			Times = new List<Time>();
+			foreach (Event e in ListOfEvents) {
+				Time NewTime = new Time();
+				NewTime.Priority = e.Priority;
+
+			}
+		}
+
+		class Time {
+			public TimeSpan Span { get; set; }
+			public Priority Priority { get; set; }
+		}
+	}
+}
