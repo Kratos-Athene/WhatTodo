fe2e07c456968ed5e6a2c47b62d9f0b118dc30d0
Matias Saarinen <matias.saarinen@aalto.fi>
2013-02-21 20:35:46 +0200

    Created and added fields with accessors

---
+++ WhatTodo/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WhatTodo
+{
+    class Event
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Name { get; set; }
+        public string Info { get; set; }
+    }
+}
