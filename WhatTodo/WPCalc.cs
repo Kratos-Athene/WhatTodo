d8c25fd717169fd8981cf868a9146fa678f6940d
Matias Saarinen <matias.saarinen@aalto.fi>
2013-02-21 20:38:15 +0200

    Almost ready
    Search appointments, translate to events.
    After callback use Events property

---
+++ WhatTodo/WPCalc.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Phone.UserData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WhatTodo
+{
+    class WPCalc
+    {
+        private Action Callback;
+        private bool Error;
+        public List<Event> Events { get; set; }
+
+        WPCalc(Action pCallback)
+        {
+            Callback = pCallback;
+        }
+
+        public void LoadUserEvents()
+        {
+            LoadAppointments();
+        }
+
+        private void LoadAppointments()
+        {
+            Appointments appoints = new Appointments();
+
+            appoints.SearchCompleted += appoints_SearchCompleted;
+
+            DateTime now = DateTime.Now;
+            DateTime end = now.AddYears(1);
+
+            appoints.SearchAsync(now, end, "Get all appointments for WhatTodo");
+        }
+
+        private void appoints_SearchCompleted(object sender, AppointmentsSearchEventArgs e)
+        {
+            IEnumerable<Event> appointEnumerable = from appoint in e.Results
+                                             select new Event {
+                                                StartTime = appoint.StartTime,
+                                                EndTime = appoint.EndTime,
+                                                Name = appoint.Subject
+                                             };
+
+            Events = appointEnumerable.ToList<Event>();
+
+            Callback.Invoke();
+        }
+    }
+}
