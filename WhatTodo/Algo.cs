0928d7ff735da000fac9c1a164fb7aaa982e8053
Olli Keskinen <oe.keskinen@gmail.com>
2013-02-22 05:15:50 +0200

    Minor bugs fixed in Algo.cs

--- WhatTodo/Algo.cs
+++ WhatTodo/Algo.cs
@@ -15,7 +15,7 @@ namespace WhatTodo {
 		public TodoList todolist;
 		public TimeList timelist;
 
-		public Algo(List<Event> pEventList, TodoList pTodoList, DateTime pNow, Action pCallback) {
+		public Algo(List<Event> pEventList, TodoList pTodoList, Action pCallback) {
 
 			//aikaa_kaikelle = kuinka paljon aikaa tarvitaan kaikkien listassa olevien toteutukseen
 			todolist = pTodoList;
@@ -38,20 +38,18 @@ namespace WhatTodo {
 			while (dl != null) {
 				//	timeRqd = DL.Duration() * 3/2
 				int time_required = (int)dl.Required.TotalMinutes;
-						//	counter = timeRqd * 2
+				//	counter = timeRqd * 2
 				int counter = time_required * 2;
-
-				
 
 				// deadlinen index, puolitettu tarvittaessa
 				int end_index = timelist.GetTimeIndex(dl.GetTimeToDeadline());
 
 				// start_index iteroidaan alaspäin
-				int start_index = end_index-1;
+				int start_index = end_index - 1;
 				// kuinka paljon tultiin alaspäin
 				int duration = 0;
 
-				while (start_index>0) {
+				while (start_index > 0) {
 					TimeList.Time element = timelist.GetElement(start_index);
 					// lisätään jokatapauksessa duration
 					duration += (int)element.Span.TotalMinutes;
@@ -60,10 +58,10 @@ namespace WhatTodo {
 						counter -= (int)element.Span.TotalMinutes;
 					}
 					// jos tultiin tarpeeksi pitkälle
-					if (counter<0) {
+					if (counter < 0) {
 						duration += counter;
 						int start_instance = dl.GetTimeToDeadline() - duration;
-						start_index = GetTimeIndex(start_instance);
+						start_index = timelist.GetTimeIndex(start_instance);
 					}
 				}
 				if (timelist.GetTimeBetween(start_index, end_index) < time_required) {
@@ -89,8 +87,7 @@ namespace WhatTodo {
 				}
 			}
 
-			//for LAZY in LAZYS
-			//	sijoita LAZY
+			pCallback.Invoke();
 		}
 
 
@@ -111,8 +108,8 @@ namespace WhatTodo {
 			// TODO FIRST
 			//sortedDL.Sort(delegate(Person p1, Person p2) { return p1.name.CompareTo(p2.name); });
 			sortedDL.Sort(delegate(TodoEvent te1, TodoEvent te2) {
-					return te1.Deadline.CompareTo(te2.Deadline);
-				});
+				return te1.Deadline.CompareTo(te2.Deadline);
+			});
 		}
 
 		//int CalculateTime(DateTime now, DateTime then) {
@@ -148,7 +145,7 @@ namespace WhatTodo {
 		}
 
 		// position as ASAP
-		public void Panic(TodoEvent te) {			
+		public void Panic(TodoEvent te) {
 			int dur = (int)te.Required.TotalMinutes;
 
 			int i = 0;
@@ -168,7 +165,7 @@ namespace WhatTodo {
 				while ((remaining = timelist.InsertElement(i, remaining, Priority.ASAP)) > 0) {
 					i++;
 					while (i < timelist.GetSize()) {
-						if (timelist.GetElement(i).Priority==Priority.FREE) {
+						if (timelist.GetElement(i).Priority == Priority.FREE) {
 							break;
 						}
 						else {
@@ -181,11 +178,11 @@ namespace WhatTodo {
 
 		public TimeList CropAndSort(int start, int end, TimeList list) {
 			List<TimeList.Time> new_list = timelist.Times.GetRange(start, end - start);
-			
+
 			// SORT
 			new_list.Sort(delegate(TimeList.Time t1, TimeList.Time t2) {
-					return (t1.Span.CompareTo((int)t2.Span.TotalMinutes));
-				});
+				return (t1.Span.CompareTo((int)t2.Span.TotalMinutes));
+			});
 			TimeList tl = new TimeList(new_list);
 			return tl;
 		}
@@ -196,18 +193,17 @@ namespace WhatTodo {
 			int i = 0;
 
 			int remaining = dur;
-				while ((remaining = timelist.InsertElement(i, remaining, Priority.ASAP)) > 0) {
-					i++;
-					while (i < list.GetSize()) {
-						if (timelist.GetElement(i).Priority==Priority.FREE) {
-							break;
-						}
-						else {
-							i++;
-						}
-					} // while etsi uusi
-				} // on vielä lisättävää
-			} // end of Position
-		}
+			while ((remaining = timelist.InsertElement(i, remaining, Priority.ASAP)) > 0) {
+				i++;
+				while (i < list.GetSize()) {
+					if (timelist.GetElement(i).Priority == Priority.FREE) {
+						break;
+					}
+					else {
+						i++;
+					}
+				} // while etsi uusi
+			} // on vielä lisättävää
+		} // end of Position
 	} // end of class
 } // end of namespace
