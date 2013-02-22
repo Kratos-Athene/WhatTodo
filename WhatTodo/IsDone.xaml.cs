d5f02582f1454d91476e3a22600b9a16c5d34101
Matias Saarinen <matias.saarinen@aalto.fi>
2013-02-22 07:02:38 +0200

    is it done page almost ready

--- WhatTodo/IsDone.xaml.cs
+++ WhatTodo/IsDone.xaml.cs
@@ -7,41 +7,15 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using System.Diagnostics;
 
 namespace WhatTodo {
 	public partial class WhatDone : PhoneApplicationPage {
-		IsDoneEvent e1 = new IsDoneEvent {
-			Name = "Do the dishes",
-			TimeString = "13.54, yesterday",
-			OriginalEvent = null,
-			ElementName = "e1"
-		};
-		IsDoneEvent e2 = new IsDoneEvent {
-			Name = "Marketing article review",
-			TimeString = "20.00, yesterday",
-			OriginalEvent = null,
-			ElementName = "e2"
-		};
-		IsDoneEvent e3 = new IsDoneEvent {
-			Name = "Code lika an animal",
-			TimeString = "8.00, today",
-			OriginalEvent = null,
-			ElementName = "e3"
-		};
-		IsDoneEvent e4 = new IsDoneEvent {
-			Name = "Do the dishes",
-			TimeString = "14.00, today",
-			OriginalEvent = null,
-			ElementName = "e4"
-		};
 
 		List<IsDoneEvent> IsDoneEvents;
 
 		public WhatDone() {
 			InitializeComponent();
-
-			IsDoneEvents = new List<IsDoneEvent> { e1, e2, e3, e4 };
-			IsDoneList.ItemsSource = IsDoneEvents;
 		}
 
 		public class IsDoneEvent {
@@ -54,21 +28,21 @@ namespace WhatTodo {
 		}
 
 		private void AcceptDone(object sender, RoutedEventArgs e) {
-			
-			List<Event> eventsToRemove = new List<Event>();
-			IsDoneEvents.ForEach(delegate(IsDoneEvent isDone) {
-				CheckBox checkBox = IsDoneList.FindName(isDone.ElementName) as CheckBox;
-				if (checkBox.IsChecked.Equals(true)) {
-					eventsToRemove.Add(isDone.OriginalEvent);
-				}
-			});
+			NavigationService.Navigate(new Uri("/WhatToDo.xaml", UriKind.Relative));
 		}
 
 		private void AllCheckBoxClicked(object sender, RoutedEventArgs e) {
 			if (AllCheckBox.IsChecked.Value) {
-				IsDoneEvents.ForEach(delegate(IsDoneEvent isDone) {
-					isDone.Remove = true;
-				});
+				((CheckBox)e1).IsChecked = true;
+				((CheckBox)e2).IsChecked = true;
+				((CheckBox)e3).IsChecked = true;
+				((CheckBox)e4).IsChecked = true;
+			}
+			else {
+				((CheckBox)e1).IsChecked = false;
+				((CheckBox)e2).IsChecked = false;
+				((CheckBox)e3).IsChecked = false;
+				((CheckBox)e4).IsChecked = false;
 			}
 		}
 
