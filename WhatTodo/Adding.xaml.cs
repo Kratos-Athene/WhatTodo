5aed8d7130d5154e653228181594b2072084b539
Lauri Lavanti <lauri.lavanti@aalto.fi>
2013-02-22 11:43:29 +0200

    Enhancing the UX

--- WhatTodo/Adding.xaml.cs
+++ WhatTodo/Adding.xaml.cs
@@ -19,11 +19,25 @@ namespace WhatTodo
 			TodoListpicker.ItemsSource = Array;
 			string[] Times = { "15 min", "30 min", "1 hour", "2 hours", "3 hours", "4 hours", "6 hours", "12 hours" };
 			TodoDuration.ItemsSource = Times;
+			TodoDLDate.IsEnabled = false;
+			TodoDLTime.IsEnabled = false;
         }
 
         private void Exit(object sender, System.EventArgs e)
         {
         	NavigationService.Navigate(new Uri("/WhatTodo.xaml", UriKind.Relative));
+        }
+
+        private void EnableDLFields(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+        	if(TodoListpicker.SelectedItem.Equals("Deadline")) {
+				TodoDLDate.IsEnabled = true;
+				TodoDLTime.IsEnabled = true;
+			}
+			else {
+				TodoDLDate.IsEnabled = false;
+				TodoDLTime.IsEnabled = false;
+			}
         }
     }
 }
