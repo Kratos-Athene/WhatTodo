bb6991960e1bc15452481cc65f510b0fbc083915
Lauri Lavanti <lauri.lavanti@aalto.fi>
2013-02-22 11:06:25 +0200

    Fixed Adding to look and feel how we wanted

--- WhatTodo/Adding.xaml.cs
+++ WhatTodo/Adding.xaml.cs
@@ -15,6 +15,10 @@ namespace WhatTodo
         public Adding()
         {
             InitializeComponent();
+			string[] Array = { "Unurgent", "ASAP", "Deadline" };
+			TodoListpicker.ItemsSource = Array;
+			string[] Times = { "15 min", "30 min", "1 hour", "2 hours", "3 hours", "4 hours", "6 hours", "12 hours" };
+			TodoDuration.ItemsSource = Times;
         }
 
         private void Exit(object sender, System.EventArgs e)
